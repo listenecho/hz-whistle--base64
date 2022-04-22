a35c3c0247f784068753bcae37bd2b3ac39b125f
listenecho <listenecho@foxmail.com>
2022-04-22 11:14:55 +0800

    feat：增加小功能

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 
 exports.uiServer = require('./lib/uiServer');
+exports.server = require('./lib/server');
+
+
+/**
+ * 统计服务
+ */
+//
+exports.statsServer = require('./lib/statsServer');
+exports.resStatsServer = require('./lib/resStatsServer');
+
+/**
+ *
+ */
+exports.rulesServer = require('./lib/rulesServer');
+exports.resRulesServer = require('./lib/resRulesServer');
+
+
 
